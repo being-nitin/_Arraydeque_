044d1cc51e2490878302f259272d314bd7d987c3
Nitin <nsinghboss02@gmail.com>
2022-03-02 19:36:05 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,20 @@
+package com.Recursion;
+import java.util.ArrayDeque;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	It is an special type of data structure in which insertion and deletion can be
+	done from both the end , and you can peek the element from both the side.
+	 */
+        ArrayDeque <Integer> aq = new ArrayDeque<>();
+        aq.offerFirst(7887);
+        aq.offerFirst(7882);
+        aq.offerFirst(7881);
+        aq.offerFirst(7883);
+        aq.offerLast(876);
+        aq.offerLast(877);
+        aq.offerLast(878);
+
+    }
+}
